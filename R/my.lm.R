1d3b3289928735d52064a79c82166fae48c39eb3
Spencer Haupert <srhaup@gmail.com>
2021-12-01 23:29:31 -0500

    work on main function

--- R/my.lm.R
+++ R/my.lm.R
@@ -2,18 +2,43 @@
 #'
 #'Fits a linear regression model using OLS
 #'
-#'@param x input value
+#'@param formula formula specifying the regression equation
 #'
-#'@return the first j principal components
+#'@param data data.frame to be used to fit the model
+#'
+#'@param subset optional logical vector specifying rows to be used to fit the model
+#'
+#'@param weights optional numeric vector to be used in a WLS regression
+#'
+#'@return A list containing the coefficients, residuals, fitted values, and more
 #'
 #'@examples
-#'
+#'get(data(mtcars))
+#'my.fit = my.lm(mpg ~ cyl, data = mtcars)
+#'my.fit$coefficients
 #'
 #'@export
 #'
 
 my.lm = function(formula, data, subset, weights, na.action) {
+  ### load pipe
+  `%>%` <- magrittr::`%>%`
+  ### check if we need to subset data
+  if (!is.null(subset)) {
+    data = data %>%
+      dplyr::filter(subset)
+  }
+  ### extract formula
+  vars = all.vars(formula)
+  nvar = length(vars)
+  y = vars[1]
+  x = vars[2:nvars]
+  ### create model.frame
+  m = model.frame(formula, data)
+  ### create dummy variables
 
+
+  ### WLS or OLS
 
 
   return(list(coefficients = coefficients, residuals = residuals, fitted.values = fitted.values,
