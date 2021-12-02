516767de30003792d23b3b5634a25f4cd1cc8136
Spencer Haupert <srhaup@gmail.com>
2021-12-01 19:39:15 -0500

    first commit

---
+++ R/my.lm.R
@@ -0,0 +1,22 @@
+#'my.lm
+#'
+#'Fits a linear regression model using OLS
+#'
+#'@param x input value
+#'
+#'@return the first j principal components
+#'
+#'@examples
+#'
+#'
+#'@export
+#'
+
+my.lm = function(formula, data, subset, weights, na.action) {
+
+
+
+  return(list(coefficients = coefficients, std.err = std.err, t.stat = t.stat,
+              p.val = p.val, residuals = residuals, fitted.values = fitted.values,
+              df.residual = df.residual, call = call, terms = terms, model = model))
+}
