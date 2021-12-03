ac4498d94bbeae45302c4c76fa851f583b59a43e
Spencer Haupert <srhaup@gmail.com>
2021-12-03 13:50:39 -0500

    my.lm almost finished. Added new function summary

--- R/my.lm.R
+++ R/my.lm.R
@@ -6,8 +6,6 @@
 #'
 #'@param data data.frame to be used to fit the model
 #'
-#'@param subset optional logical vector specifying rows to be used to fit the model
-#'
 #'@param weights optional numeric vector to be used in a WLS regression
 #'
 #'@return A list containing the coefficients, residuals, fitted values, and more
@@ -20,27 +18,64 @@
 #'@export
 #'
 
-my.lm = function(formula, data, subset, weights, na.action) {
+my.lm = function(formula, data, weights) {
   ### load pipe
   `%>%` <- magrittr::`%>%`
-  ### check if we need to subset data
-  if (!is.null(subset)) {
-    data = data %>%
-      dplyr::filter(subset)
-  }
   ### extract formula
   vars = all.vars(formula)
   nvar = length(vars)
-  y = vars[1]
-  x = vars[2:nvars]
+  y.name = vars[1]
+  x.names = vars[2:nvar]
   ### create model.frame
-  m = model.frame(formula, data)
-  ### create dummy variables
-
-
+  mf= stats::model.frame(formula = formula, data = data)
+  y = mf[,1]
+  ### create design matrix
+  mm = stats::model.matrix(formula, mf)
+  n = nrow(mm)
+  p = ncol(mm)
   ### WLS or OLS
-
-
+  if(!missing("weights")) {
+    #use WLS estimator
+    w = diag(weights)
+    coefficients = solve(t(mm) %*% w %*% mm) %*% (t(mm) %*% w %*% y)
+    fitted.values = mm %*% coefficients
+    residuals = y - fitted.values
+    sse = as.numeric(t(y) %*% w %*% y - t(y) %*% w %*% mm %*% solve(t(mm) %*% w %*% mm) %*% (t(mm) %*% w %*% y))
+    # R uses this different method for computing ssy in WLS (very confusing)
+    ssy = sum(weights*(y^2)) - (1/sum(weights))*(sum(weights*y)^2)
+    ssr = ssy-sse
+    mse = sse/(n-p)
+    msr = ssr/(p-1)
+    msy = ssy/(n-1)
+    var_y = mse * solve(w)
+    var_beta = solve(t(mm) %*% w %*% mm) %*% t(mm) %*% w %*% var_y %*% t(w) %*% mm %*% solve(t(mm) %*% w %*% mm)
+    sigma = sqrt(mse)
+    se_beta = sqrt(diag(var_beta))
+  } else {
+    #use OLS estimator
+    coefficients = solve(t(mm) %*% mm) %*% (t(mm) %*% y)
+    fitted.values = mm %*% coefficients
+    residuals = y - fitted.values
+    sse = as.numeric(crossprod(residuals))
+    ssr = as.numeric(crossprod(fitted.values - mean(y)))
+    ssy = as.numeric(crossprod(y - mean(y)))
+    mse = sse/(n-p)
+    msr = ssr/(p-1)
+    msy = ssy/(n-1)
+    var_beta = mse * solve(t(mm) %*% mm)
+    sigma = sqrt(mse)
+    se_beta = sqrt(diag(var_beta))
+  }
+  ### calculate other relevant things
+  r2 = ssr/ssy
+  adj.r2 = 1 - ((sse/(n-p))/(ssy/(n-1)))
+  #t stats
+  t = coefficients/se_beta
+  t.p.vals = ifelse(t > 0, (2* stats::pt(t, df = 29, lower.tail = FALSE)), (2* stats::pt(t, df = 29, lower.tail = TRUE)))
+  #f stat
+  f = msr/mse
+  f.p.val = stats::pf(f, p-1, n-p, lower.tail = FALSE)
   return(list(coefficients = coefficients, residuals = residuals, fitted.values = fitted.values,
-              df.residual = df.residual, call = call, terms = terms, model = model))
+              mse = mse, se_beta = se_beta, r2 = r2, adj.r2 = adj.r2, t = t, t.p.vals = t.p.vals,
+              f = f, f.p.val = f.p.val,df.residual = n - p, formula = formula, design.mat = mm))
 }
---
+++ R/my.summary.R
@@ -0,0 +1,50 @@
+#'my.summary
+#'
+#'cats regression summary from output of my.lm function
+#'
+#'@param x output from my.lm function
+#'
+#'@return no return value - this function only cats a summary of the regression
+#'
+#'@examples
+#'get(data(mtcars))
+#'my.fit = my.lm(mpg ~ cyl, data = mtcars)
+#'summary(my.fit)
+#'
+#'@export
+#'
+
+my.summary = function(x){
+  #cat summary
+  cat("Call:\n")
+  print(x$formula)
+  cat("\n")
+  #get data on residuals
+  mi = min(x$residuals)
+  q1 = stats::quantile(x$residuals, 0.25)
+  med = stats::median(x$residuals)
+  q3 = stats::quantile(x$residuals, 0.75)
+  ma = max(x$residuals)
+  res_vec = round(c(mi, q1, med, q3, ma),4)
+  names(res_vec) = c("Min", "1Q", "Median", "3Q", "Max")
+  cat("Residuals: \n")
+  print(res_vec)
+  cat("\n")
+  #create summary table
+  tbl = data.frame(Estimate = as.numeric(x$coefficients), Std.Error = as.numeric(x$se_beta),
+                   t.value = as.numeric(x$t), p.value = as.numeric(x$t.p.vals))
+  rownames(tbl) = rownames(x$coefficients)
+  sigcodes = ifelse(x$t.p.value < 0.001, "***",
+                    ifelse(x$t.p.value < 0.01, "**",
+                           ifelse(x$t.p.value < 0.05, "*",
+                                  ifelse(x$t.p.value < 0.1, ".", ""))))
+  tbl = signif(rbind(tbl,sigcodes),4)
+  cat("Coefficients:\n")
+  print(tbl)
+  cat("---\n")
+  cat("Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1\n")
+  cat("\n")
+  cat(paste0("Residual standard error: ", round(sqrt(x$mse),4)," on ", round(x$df.residual,4), " degrees of freedom\n"))
+  cat(paste0("Multiple R-squared: ", round(x$r2,4)," ,      Adjusted R-squared: ", round(x$adj.r2,4), "\n"))
+  cat(paste0("F-statistic: ", round(x$f,4)," on ", length(x$coefficients) - 1, " and ", x$df.residual, " DF,  p-value: ", signif(x$f.p.val,4)))
+}
